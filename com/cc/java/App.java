952b2dcf49e6f4159da4f15d1b363b45da9247f5
Vladimir-Haar <haar.vlad@gmx.de>
2025-01-16 10:44:03 +0100

    Objekte durch new erzeugen.

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,13 +1,13 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
 
+        // 1. Iteration: Objekte durch neu erzeugen
+        System.out.println(new Cat());
+        System.out.println(new Cat());
 
     }
 
-   
-
 }
-
